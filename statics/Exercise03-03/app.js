6e294b195114fe51c41fc69421a1d4440d6a38da
gagan0183 <g.deepsingh1@gmail.com>
2017-12-04 19:01:54 +0530

    factory

--- statics/Exercise03-03/app.js
+++ statics/Exercise03-03/app.js
@@ -1,14 +1,48 @@
 var app = angular.module('maintenance', ['ngRoute']);
 app.config(function($routeProvider) {
     $routeProvider.when('/locations', {
-        templateUrl: 'views/locations.html'
+        templateUrl: 'views/locations.html',
+        controller: 'LocationsCtrl'
     })
     .when('/sites', {
-        templateUrl: 'views/sites.html'
+        templateUrl: 'views/sites.html',
+        controller: 'SitesCtrl'
     })
     .otherwise({
-        templateUrl: 'views/main.html'
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
     });
 });
-app.controller('AdminCtrl', function($scope) {
+app.factory('currentSpot', function() {
+    var activeMenu = '';
+    var titleText = '';
+    return {
+        setCurrentSpot: function(menu, title) {
+            activeMenu = menu;
+            titleText = title; 
+        },
+        getActiveMenu: function() {
+            return activeMenu;
+        },
+        getTitleText: function() {
+            return titleText;
+        }
+    }
+});
+app.controller('AdminCtrl', function($scope, currentSpot) {
+    $scope.getTitle = function() {
+        return currentSpot.getTitleText();
+    },
+    $scope.isActive = function(activeMenu) {
+        return currentSpot.getActiveMenu() === activeMenu;
+    }
+});
+app.controller('MainCtrl', function($scope, currentSpot) {
+    currentSpot.setCurrentSpot('', '');
+});
+app.controller('LocationsCtrl', function($scope, currentSpot) {
+    currentSpot.setCurrentSpot('Locations', 'Manage the list of locations');
+});
+app.controller('SitesCtrl', function($scope, currentSpot) {
+    currentSpot.setCurrentSpot('Sites', 'Manage the list of sites');
 });
