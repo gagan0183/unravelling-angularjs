917c6defab4662c9bbb2254286c3be09b2462e1d
gagan0183 <g.deepsingh1@gmail.com>
2017-12-10 18:35:19 +0530

    service http

--- statics/Exercise03-06/modules/maintenance.js
+++ statics/Exercise03-06/modules/maintenance.js
@@ -1,6 +1,15 @@
 var maintenance = angular.module('maintenance', []);
 app.controller('LocationsCtrl', function($scope, currentSpot) {
-    
+    var selectedId = -1;
+    var addFlag = false;
+    var editFlag = false;
+    var removeFlag = false;
+    var rings = [];
+    $scope.model = {};
+    refresh();
+    $scope.isBusy = function(id) {
+
+    }
 });
 app.controller('SitesCtrl', function($scope, currentSpot) {
     
